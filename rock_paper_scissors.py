1dc9676a0640b737c3f102b080732665f2a3305b
Mike Loyet <mikeloyet@gmail.com>
2023-01-30 05:43:14 -0600

    Add variables and list to track user and computer wins and number of options.

---
+++ rock_paper_scissors.py
@@ -0,0 +1,9 @@
+import random
+
+# Need to track number of user and computer wins.
+user_wins = 0
+computer_wins = 0
+
+# List to contain number of options.
+options = ["rock", "paper", "scissors"]
+
