1fc000d40e59e48c5ea53c5632e77bb520650fe4
Mike Loyet <mikeloyet@gmail.com>
2023-01-30 05:50:50 -0600

    Create loop to check user input and check if it's valid

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -7,3 +7,13 @@ computer_wins = 0
 # List to contain number of options.
 options = ["rock", "paper", "scissors"]
 
+# Create loop to check user input
+while True:
+    # Get user input and check if it's valid
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        break
+
+    if user_input not in options:
+        continue
+
