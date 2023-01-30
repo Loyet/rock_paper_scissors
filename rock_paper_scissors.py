d8c8d25a633bf6d5e8ae2d354e6607e27ef1a37c
Mike Loyet <mikeloyet@gmail.com>
2023-01-30 06:03:53 -0600

    Add if blocks for outcomes.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -22,3 +22,16 @@ while True:
     # Rock: 0, Paper: 1, Scissors: 2
     computer_pick = options[random_number]
     print(f"Computer picked: {computer_pick}")
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You won!")
+        user_wins += 1
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
+    else:
+        print("You lose!")
+        computer_wins =+ 1
